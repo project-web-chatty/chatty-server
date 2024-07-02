9fb3a44dc67784efedc3acb544bee22b6b20a5d8
suhyeonOh <sh020119@snu.ac.kr>
2024-07-02 14:41:20 +0900

    add .env file

--- src/main/java/com/messenger/chatty/connect/HealthCheckController.java
+++ src/main/java/com/messenger/chatty/connect/HealthCheckController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// health check from ELB
 @RestController
 public class HealthCheckController {
     @GetMapping("/api/isHealthy")
