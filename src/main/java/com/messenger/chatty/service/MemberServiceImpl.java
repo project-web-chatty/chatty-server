82555b4394a7b33a1a0b52a970fb4ecd873f896c
suhyeonOh <sh020119@snu.ac.kr>
2024-07-12 02:50:52 +0900

    refactor: customize exception constructor

--- src/main/java/com/messenger/chatty/service/MemberServiceImpl.java
+++ src/main/java/com/messenger/chatty/service/MemberServiceImpl.java
@@ -10,6 +10,7 @@ import com.messenger.chatty.dto.response.workspace.WorkspaceBriefDto;
 import com.messenger.chatty.entity.Channel;
 import com.messenger.chatty.entity.Member;
 import com.messenger.chatty.entity.Workspace;
+import com.messenger.chatty.exception.custom.CustomNoSuchElementException;
 import com.messenger.chatty.exception.custom.DuplicatedNameException;
 import com.messenger.chatty.repository.ChannelRepository;
 import com.messenger.chatty.repository.MemberRepository;
@@ -38,7 +39,7 @@ public class MemberServiceImpl implements MemberService{
     public MyProfileDto signup(MemberJoinRequestDto memberJoinRequestDTO){
 
         if(memberRepository.existsByUsername(memberJoinRequestDTO.getUsername()))
-            throw new DuplicatedNameException("duplicated username : "+ memberJoinRequestDTO.getUsername());
+            throw new DuplicatedNameException(memberJoinRequestDTO.getUsername(),"username");
 
         memberJoinRequestDTO.encodePassword(bcrptPasswordEncoder.encode(memberJoinRequestDTO.getPassword()));
         Member me = Member.from(memberJoinRequestDTO);
@@ -56,14 +57,14 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public MemberBriefDto getMemberProfileByMemberId(Long memberId) {
         Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoSuchElementException("there is no member whose member_id is " + memberId));
+                .orElseThrow(() -> new CustomNoSuchElementException("id",memberId,"회원"));
         return  CustomConverter.convertMemberToBriefDto(member);
     }
 
     @Override
     public MyProfileDto getMyProfileByUsername(String username) {
         Member me = memberRepository.findByUsername(username)
-                .orElseThrow(() -> new NoSuchElementException("there is no member whose username is " + username));
+                .orElseThrow(() -> new CustomNoSuchElementException("username",username,"회원"));
         List<Workspace> myWorkspaces = workspaceRepository.findWorkspacesByMemberId(me.getId());
 
         return CustomConverter.convertMemberToDto(me,myWorkspaces);
@@ -73,7 +74,7 @@ public class MemberServiceImpl implements MemberService{
     public MyProfileDto updateMyProfile(String targetUsername, MemberUpdateRequestDto memberUpdateRequestDto){
 
         Member me = memberRepository.findByUsername(targetUsername)
-                .orElseThrow(() -> new NoSuchElementException("there is no member whose username is " + targetUsername));
+                .orElseThrow(() -> new CustomNoSuchElementException("username",targetUsername,"회원"));
 
         String name = memberUpdateRequestDto.getName();
         String nickname = memberUpdateRequestDto.getNickname();
@@ -93,21 +94,21 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public void deleteMeByUsername(String username){
         Member me = memberRepository.findByUsername(username)
-                .orElseThrow(() -> new NoSuchElementException("there is no member whose username is " + username));
+                .orElseThrow(() -> new CustomNoSuchElementException("username",username,"회원"));
         memberRepository.deleteByUsername(username);
     }
 
     @Override
     public void deleteMeById(Long id) {
         Member me = memberRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("there is no member whose id is " + id));
+                .orElseThrow(() -> new CustomNoSuchElementException("id",id,"회원"));
         memberRepository.delete(me);
     }
 
     @Override
     public List<WorkspaceBriefDto> getMyWorkspaces(String username) {
         Member me = memberRepository.findByUsername(username)
-                .orElseThrow(() -> new NoSuchElementException("there is no member whose username is " + username));
+                .orElseThrow(() -> new CustomNoSuchElementException("username",username,"회원"));
         List<Workspace> myWorkspaces = workspaceRepository.findWorkspacesByMemberId(me.getId());
         return myWorkspaces.stream().map(CustomConverter::convertWorkspaceToBriefDto).toList();
     }
@@ -115,8 +116,9 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public List<ChannelBriefDto> getMyChannels(String username, String workspaceName) {
         Member me = memberRepository.findByUsername(username)
-                .orElseThrow(() -> new NoSuchElementException("there is no member whose username is " + username));
-        Workspace workspace = workspaceRepository.findByName(workspaceName).orElseThrow(() -> new NoSuchElementException("dsffds"));
+                .orElseThrow(() -> new CustomNoSuchElementException("username",username,"회원"));
+        Workspace workspace = workspaceRepository.findByName(workspaceName)
+                .orElseThrow(() -> new CustomNoSuchElementException("이름",workspaceName,"워크스페이스"));
         List<Channel> channels = channelRepository.findByWorkspaceIdAndMemberId(workspace.getId(), me.getId());
         return channels.stream().map(CustomConverter::convertChannelToBriefDto).toList();
     }
