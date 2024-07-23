7e8537e07591a75d2a78da6b60513755869b8789
hann <wjdgks1233@gmail.com>
2024-07-23 10:20:56 +0900

    [Refactor/#5] remove channelJoinRepository class

--- src/main/java/com/messenger/chatty/repository/ChannelJoinRepository.java
+++
@@ -1,6 +0,0 @@
-package com.messenger.chatty.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChannelJoinRepository extends JpaRepository<ChannelJoin,Long> {
-}
