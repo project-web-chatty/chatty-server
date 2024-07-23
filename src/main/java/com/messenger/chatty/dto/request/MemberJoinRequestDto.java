456b0ce4ab11b7cc86eb79aaf2e3abb5448f9955
hann <wjdgks1233@gmail.com>
2024-07-23 16:55:15 +0900

    [Test/#6] test environment setting(databasecleanup.class & mocksecurity.class)

--- src/main/java/com/messenger/chatty/dto/request/MemberJoinRequestDto.java
+++ src/main/java/com/messenger/chatty/dto/request/MemberJoinRequestDto.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
-@Getter
-@Setter
+@Data
+@Builder
 public class MemberJoinRequestDto {
     @NotBlank(message = "id는 필수 입력사항입니다.")
     @Size(min = 5,max = 20, message = "아이디는 최소 5자에서 최대 20자까지 가능합니다.")
