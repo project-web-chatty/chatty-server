7858c119076ad256ad1f6f86484535c9456f9ed8
hann <wjdgks1233@gmail.com>
2024-07-27 11:49:14 +0900

    [Test/#6] test workspace service(C,U)

--- src/main/java/com/messenger/chatty/dto/request/WorkspaceUpdateRequestDto.java
+++ src/main/java/com/messenger/chatty/dto/request/WorkspaceUpdateRequestDto.java
@@ -1,11 +1,13 @@
 package com.messenger.chatty.dto.request;
 
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class WorkspaceUpdateRequestDto {
     private String description;
 }
