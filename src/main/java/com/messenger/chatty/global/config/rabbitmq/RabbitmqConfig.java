c4f224ff0eed8891e55e56c746f263df5a2f1fe5
hann <wjdgks1233@gmail.com>
2024-08-09 21:39:06 +0900

    [Feat/#17] rabbitmqConfig.class

---
+++ src/main/java/com/messenger/chatty/global/config/rabbitmq/RabbitmqConfig.java
@@ -0,0 +1,69 @@
+package com.messenger.chatty.global.config.rabbitmq;
+
+import org.springframework.amqp.core.*;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitmqConfig {
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange("message.exchange");
+    }
+
+    @Bean
+    Queue queue() {
+        return new Queue("message.queue", true, false, false);
+    }
+
+
+    @Bean
+    Binding binding(TopicExchange topicExchange, Queue queue) {
+        return BindingBuilder.bind(queue).to(topicExchange).with("channel.*");
+    }
+
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+
+    @Bean
+    MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+}
