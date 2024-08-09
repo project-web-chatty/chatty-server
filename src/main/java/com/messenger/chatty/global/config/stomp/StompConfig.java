23d833ac5c7357ceaf9a4328c1104c1de45fb986
hann <wjdgks1233@gmail.com>
2024-08-09 21:24:10 +0900

    [Feat/#17] create stompConfig.class

---
+++ src/main/java/com/messenger/chatty/global/config/stomp/StompConfig.java
@@ -0,0 +1,30 @@
+package com.messenger.chatty.global.config.stomp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp/chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
+    }
+
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setPathMatcher(new AntPathMatcher("."));
+        registry.setApplicationDestinationPrefixes("/pub");
+
+        //registry.enableSimpleBroker("/sub");
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
+    }
+}
