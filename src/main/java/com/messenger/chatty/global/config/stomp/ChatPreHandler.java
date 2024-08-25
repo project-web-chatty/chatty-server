acfff4d7f23d79b51d9c7cd9ffe54bed59b97dd4
hann <wjdgks1233@gmail.com>
2024-08-25 21:51:42 +0900

    [Feat/#22] compelete security when connect & subscribe channel in ChatPreHandler

--- src/main/java/com/messenger/chatty/global/config/stomp/ChatPreHandler.java
+++ src/main/java/com/messenger/chatty/global/config/stomp/ChatPreHandler.java
@@ -1,37 +1,22 @@
 package com.messenger.chatty.global.config.stomp;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.messenger.chatty.domain.channel.service.ChannelService;
-import com.messenger.chatty.domain.member.dto.response.MyProfileDto;
-import com.messenger.chatty.domain.member.service.MemberService;
-import com.messenger.chatty.domain.refresh.dto.response.TokenResponseDto;
-import com.messenger.chatty.global.presentation.ErrorStatus;
-import com.messenger.chatty.global.presentation.exception.GeneralException;
-import com.messenger.chatty.global.presentation.exception.custom.MemberException;
 import com.messenger.chatty.global.util.WebSocketUtil;
 import com.messenger.chatty.security.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageDeliveryException;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-import static org.springframework.messaging.simp.stomp.StompCommand.*;
-import static org.springframework.messaging.simp.stomp.StompCommand.UNSUBSCRIBE;
 
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @Component
@@ -46,25 +31,51 @@ public class ChatPreHandler implements ChannelInterceptor {
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor headerAccessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
         StompCommand command = headerAccessor.getCommand();
-        log.info("1");
         log.info("message = {}", message.getPayload());
         log.info("message header = {}", message.getHeaders());
 
-        //only entrance
-        if(command.equals(SUBSCRIBE) || command.equals(CONNECT)) {
-            log.info("2");
-            DecodedJWT decodedJWT = authService.decodeToken(WebSocketUtil.getAccessToken(headerAccessor), "access");
-            log.info("3");
-            String username =  decodedJWT.getSubject();
-            Long channelId = WebSocketUtil.getChannelId(headerAccessor);
-            boolean validated = channelService.validateEnterChannel(channelId, username);
-            log.info("4");
-            if (!validated) {
-                log.info("validation failed");
-                //TODO exception of chat
+        if (command != null) {
+            switch (command) {
+                case CONNECT -> {
+                    DecodedJWT decodedJWT = authService.decodeToken(WebSocketUtil.getAccessToken(headerAccessor), "access");
+                    String username =  decodedJWT.getSubject();
+                    Long channelId = WebSocketUtil.getChannelId(headerAccessor);
+                    boolean validated = channelService.validateEnterChannel(channelId, username);
+                    if (!validated) {
+                        log.info("validation failed");
+                        //TODO exception of chat
+                    }
+                    headerAccessor.getSessionAttributes().put("username", username);
+                    headerAccessor.getSessionAttributes().put("channelId", channelId);
+                }
+                case SUBSCRIBE -> {
+                    String destination = headerAccessor.getDestination();
+                    String username = String.valueOf(headerAccessor.getSessionAttributes().get("username"));
+                    Long channelId = Long.valueOf(String.valueOf(headerAccessor.getSessionAttributes().get("channelId")));
+                    log.info("username = {}", username);
+                    log.info("SUBSCRIBE destination = {}", destination);
+                    log.info("channelId extracted = {}", channelId);
+
+                    // 추가적인 검증 로직
+                    if (channelId == null || !channelService.validateEnterChannel(channelId, username)) {
+                        log.error("Invalid channelId or access denied: channelId = {}", channelId);
+                        throw new MessageDeliveryException("Access denied or invalid channel ID");
+                    }
+                }
+                default -> {
+                    return message;
+                }
             }
-            log.info("5");
-            headerAccessor.getSessionAttributes().put("username", username);
+        }else{
+            SimpMessageType messageType = headerAccessor.getMessageType();
+            switch (messageType) {
+                case HEARTBEAT -> {
+                    log.info("heartbeat");
+                }
+                default -> {
+                    return message;
+                }
+            }
         }
         return message;
     }
