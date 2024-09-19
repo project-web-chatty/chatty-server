0cb7970d515b8b537b538ed7ab184e74ba48d774
suhyeonOh <sh020119@snu.ac.kr>
2024-09-19 12:30:18 +0900

    change settings for docker build

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN gradle build --no-daemon -x test
 
 FROM openjdk:17-jdk-alpine
 
-COPY --from=build /home/gradlge/project/build/libs/app.jar /app.jar
+COPY --from=build /home/gradle/project/build/libs/app.jar /app.jar
 
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/com/messenger/chatty/global/config/stomp/StompConfig.java
+++ src/main/java/com/messenger/chatty/global/config/stomp/StompConfig.java
@@ -30,7 +30,13 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/pub");
 
         //registry.enableSimpleBroker("/sub");
-        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
+                .setRelayPort(61613)
+                .setVirtualHost("/")
+                .setClientLogin("guest")
+                .setClientPasscode("guest")
+                .setRelayHost("rabbitmq"); // for docker internal network
+
     }
 
     @Override
