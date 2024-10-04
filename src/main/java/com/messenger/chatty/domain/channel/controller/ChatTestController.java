699c2b67fe45fbf1fc2dc179d64cdefba0e7ec92
suhyeonOh <sh020119@snu.ac.kr>
2024-10-04 20:24:07 +0900

    config: deploy-config-3

--- src/main/java/com/messenger/chatty/domain/channel/controller/ChatTestController.java
+++ src/main/java/com/messenger/chatty/domain/channel/controller/ChatTestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/chat/test")
 public class ChatTestController {
 
