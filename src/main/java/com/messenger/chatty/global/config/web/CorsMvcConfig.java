9afab90ca5362063d24d6d9aa4284214d6da13c9
suhyeonOh <sh020119@snu.ac.kr>
2024-11-03 16:37:49 +0900

    fix : corsMapping 변경

--- src/main/java/com/messenger/chatty/global/config/web/CorsMvcConfig.java
+++ src/main/java/com/messenger/chatty/global/config/web/CorsMvcConfig.java
@@ -1,6 +1,5 @@
 package com.messenger.chatty.global.config.web;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,8 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://ec2-3-34-211-45.ap-northeast-2.compute.amazonaws.com:3000","http://localhost:3000", "https://apic.app");
+                .allowedOrigins("http://ec2-3-34-211-45.ap-northeast-2.compute.amazonaws.com:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 추가
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 쿠키 및 인증 정보를 포함할 수 있도록 설정
     }
 }
